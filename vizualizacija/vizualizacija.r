81935075b3e39c6c2e5fdabd902574ca9628e267
larajagodnik <lara.jagodnik@gmail.com>
2019-03-01 23:51:31 +0100

    dodane knjiznice, zemljevid st.medalj na prebivalca

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,10 +7,21 @@ library(reshape2)
 library(ggplot2)
 library(munsell)
 library(scales)
+library(StatMeasures)
+library(lubridate)
 
+rezultati.tekaske$rezultat <- period_to_seconds(hms(rezultati.tekaske$rezultat))
+t1 <- rezultati.tekaske %>% filter(disciplina=="100 m", spol=="Ženski", uvrstitev==1)
+t1$sprememba <- diff(t1$rezultat)/t1$rezultat[-nrow(t1$rezultat)]
+diff(data)/data[-nrow(data),] * 100
 
+#razvoj tekaskih disciplin zenske
 ggplot(data=rezultati.tekaske %>% filter(uvrstitev==1, spol=="Ženski"), aes(x=leto, y=rezultat, group=disciplina, color=disciplina)) + geom_line()
+
+#razvoj sprinterskih disciplin moski
 ggplot(data=moski.sprint%>% filter(POS==1), aes(x=leto, y=MARK, group=disciplina, color=disciplina)) + geom_line()
+
+#primerjava reakcijskih casov po disciplinah
 ggplot(data=moski.sprint%>% filter(POS==1), aes(x=leto, y=get('Reaction Time'), group=disciplina, color=disciplina)) + geom_line() +  labs(y = "Reaction time")
 
 # Uvozimo zemljevid sveta
@@ -23,6 +34,8 @@ zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.natural
 # spremenit moras imena drzav v tabeli, da se bodo ujemala
 # st.medalj$drzava %in% zemljevid$SOVEREIGNT
 # bivših držav za katere imamo podatke ne bo na zemljevidu 
+
+
 st.medalj <- medalje %>% group_by(drzava) %>% summarise(st_medalj=sum(stevilo))
 
 #zemljevid: stevilo medalj
@@ -41,8 +54,14 @@ ggplot() + geom_polygon(data=left_join(zemljevid,st.medalj, by=c("SOVEREIGNT"="d
 
 #zemljevid: stevilo medalj na prebivalca
 medalje.na.preb <- left_join(x=st.medalj, y=populacija, by="drzava")
-medalje.na.preb$medalje_na_preb <- (medalje.na.preb$st_medalj / medalje.na.preb$prebivalstvo)*1000000000
+medalje.na.preb$medalje_na_preb <- (medalje.na.preb$st_medalj / medalje.na.preb$prebivalstvo)
+medalje.na.preb$odstopanje <- medalje.na.preb$medalje_na_preb - mean(medalje.na.preb$medalje_na_preb)
+medalje.na.preb$skupina <- decile(medalje.na.preb$odstopanje)
 
-ggplot() + geom_polygon(data=left_join(zemljevid, medalje.na.preb, by=c("SOVEREIGNT"="drzava")),
-                        aes(x=long, y=lat, group=group, fill=medalje_na_preb)) +
-  scale_fill_continuous(limits = c(0, 10000), breaks = c(100, 1000, 3000, 5000, 6250, 7500, 8750, 10000))
+ggplot() + geom_polygon(data=left_join(zemljevid,medalje.na.preb, by=c("SOVEREIGNT"="drzava")),
+                        aes(x=long, y=lat, group=group, fill=factor(skupina))) +labs(fill="Skupina")
+
+# zemljevid z zvezno barvno lestvico
+# ggplot() + geom_polygon(data=left_join(zemljevid, medalje.na.preb, by=c("SOVEREIGNT"="drzava")),
+#                         aes(x=long, y=lat, group=group, fill=medalje_na_preb)) +
+#   scale_fill_continuous(limits = c(0, 10000), breaks = c(100, 1000, 3000, 5000, 6250, 7500, 8750, 10000))
