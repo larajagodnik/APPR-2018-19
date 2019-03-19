6b71a63b06188f90f009024069b0e6aeec7208d7
larajagodnik <lara.jagodnik@gmail.com>
2019-03-19 23:18:43 +0100

    dokoncan shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,96 +1,6 @@
-# library(shiny)
-# 
-# shinyServer(function(input, output) {
-#   
-#   output$mymap <- renderLeaflet({
-# 
-#     zemljevid.datas <- sample.data
-#     if (!is.null(input$spol) && input$spol %in% sample.data$spol) {
-#       zemljevid.datas <- sample.data %>% filter(spol==input$spol)
-#     }
-#     if (!is.null(input$leto) && input$leto %in% sample.data$leto) {
-#       zemljevid.datas <- zemljevid.datas %>% filter(leto==input$leto)
-#     }    
-#     if(!is.null(input$disciplina) && input$disciplina %in% sample.data$disciplina){
-#       zemljevid.datas <- zemljevid.datas %>% filter(disciplina==input$disciplina)
-#     }
-#     
-#     m <- leaflet(data = zemljevid.datas) %>%
-#       addTiles() %>%
-#       addMarkers(lng = zemljevid.datas$Longitude,
-#                  lat = zemljevid.datas$Latitude ,
-#                  popup = paste("Tekmovalec:", zemljevid.datas$ATHLETE ,"<br>",
-#                                "Država:", zemljevid.datas$admin, "<br>",
-#                                "Leto:", zemljevid.datas$leto,"<br>",
-#                                "Disciplina:", zemljevid.datas$disciplina,"<br>",
-#                                "Uvrstitev:", zemljevid.datas$POS),
-#                  clusterOptions = markerClusterOptions(freezeAtZoom=4))
-#     
-#     
-#   })
-#   
-#   
-#   
-#   
-#   
-#   output$graf.sprememba.rezultata <- renderPlot({
-#     
-#     main <- "Sprememba rezultatov glede na leto 2005"
-#     t <- rezultati
-#     
-#     if (!is.null(input$tip) && input$tip=="Tehnicne") {
-#       t <- t %>% filter(disciplina %in% levels(factor(rezultati.tehnicne$disciplina)))
-#     }
-#     # if (!is.null(input$disciplina.graf) && input$disciplina.graf %in% levels(factor(rezultati$disciplina))) {
-#     #   t <- t %>% filter(disciplina==input$disciplina.graf)
-#     # }
-#     if (!is.null(input$spol.graf) && input$spol.graf %in% levels(factor(rezultati$spol))) {
-#       t <- t %>% filter(spol==input$spol.graf)
-#     }
-#     if(!is.null(input$tip) && input$tip=="Tekaske"){
-#       t <- t %>% filter(disciplina %in% levels(factor(rezultati.tekaske$disciplina)))
-#     }
-#     
-#     ggplot(data=t, mapping = aes(x=factor(leto), y=sprememba, group=disciplina, color=disciplina)) +
-#       geom_line(size=2) +
-#       labs(x="Leto", y="Sprememba", color="Disciplina")
-#   })
-#   
-#   
-#   
-#   output$spol <- renderUI(
-#     selectInput("spol", label="Izberi spol",
-#                 choices=c("Vse", levels(factor(sample.data$spol))))
-#   )
-#   output$disciplina <- renderUI(
-#     selectInput("disciplina", label="Izberi disciplino",
-#                 choices=c("Vse", levels(factor(sample.data$disciplina))))
-#   )
-#   output$leto <- renderUI(
-#     selectInput("leto", label="Izberi leto",
-#                 choices=c("Vse", levels(factor(sample.data$leto))))
-#   )
-#   output$spol.graf <- renderUI(
-#     selectInput("spol.graf", label="Izberi spol",
-#                 choices=levels(factor(rezultati$spol)))
-#   )
-#   # output$disciplina.graf <- renderUI(
-#   #   selectInput("disciplina.graf", label="Izberi disciplino",
-#   #               choices=c("Vse", levels(factor(rezultati$disciplina))))
-#   # )
-#   output$tip <- renderUI(
-#     selectInput("tip", label="Izberi tip",
-#                 choices=c("Vse", levels(factor(rezultati$kategorija))))
-#   )
-#   
-#   
-#   
-# })
-
-
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output,session) {
   
   output$mymap <- renderLeaflet({
     
@@ -113,7 +23,8 @@ shinyServer(function(input, output) {
                                "Država:", zemljevid.datas$admin, "<br>",
                                "Leto:", zemljevid.datas$leto,"<br>",
                                "Disciplina:", zemljevid.datas$disciplina,"<br>",
-                               "Uvrstitev:", zemljevid.datas$POS),
+                               "Uvrstitev:", zemljevid.datas$POS,"<br>" ,
+                               "Rezultat:", zemljevid.datas$MARK,"<br>"),
                  clusterOptions = markerClusterOptions(freezeAtZoom=4))
     
     
@@ -124,9 +35,8 @@ shinyServer(function(input, output) {
   
   
   output$graf.sprememba.rezultata <- renderPlot({
-    
-    main <- "Sprememba rezultatov glede na leto 2005"
-    t <- rezultati %>% filter(disciplina %in% input$discipline)
+
+    t <- rezultati %>% filter(disciplina %in% input$discipline_tehnicne | rezultati$disciplina %in% input$discipline_tekaske)
     
     if (!is.null(input$tip) && input$tip=="Tehnicne") {
       t <- t %>% filter(disciplina %in% levels(factor(rezultati.tehnicne$disciplina)))
@@ -137,10 +47,11 @@ shinyServer(function(input, output) {
     if (!is.null(input$spol.graf) && input$spol.graf %in% levels(factor(rezultati$spol))) {
       t <- t %>% filter(spol==input$spol.graf)
     }
-    # if(!is.null(input$tip) && input$tip=="Tekaske"){
-    #   t <- t %>% filter(disciplina %in% levels(factor(rezultati.tekaske$disciplina)))
-    # }
+    if(!is.null(input$tip) && input$tip=="Tekaske"){
+      t <- t %>% filter(disciplina %in% levels(factor(rezultati.tekaske$disciplina)))
+    }
     if(!is.null(input$discipline) && input$discipline %in% levels(factor(rezultati$disciplina))){
+      print(input$discipline)
       t <- t %>% filter(disciplina %in% levels(factor(rezultati$disciplina)))
     }
     ggplot(data=t, mapping = aes(x=factor(leto), y=sprememba, group=disciplina, color=disciplina)) +
@@ -170,10 +81,10 @@ shinyServer(function(input, output) {
   #   selectInput("disciplina.graf", label="Izberi disciplino",
   #               choices=c("Vse", levels(factor(rezultati$disciplina))))
   # )
-  # output$tip <- renderUI(
-  #   selectInput("tip", label="Izberi tip",
-  #               choices=c("Vse", levels(factor(rezultati$kategorija))))
-  # )
+  output$tip <- renderUI(
+    selectInput("tip", label="Izberi tip",
+                choices=c("Vse", levels(factor(rezultati$kategorija))))
+  )
   
   
   
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,34 +1,3 @@
-# library(shiny)
-# 
-# shinyUI(fluidPage(
-#   
-#   titlePanel("Rezultati svetovnih prvenstev"),
-#   
-#   tabsetPanel(
-#     tabPanel("Graf",
-#              sidebarPanel(
-#                
-#                uiOutput("spol.graf"),
-#                uiOutput("tip"),
-#                uiOutput("disciplina.graf")),
-#              mainPanel(plotOutput("graf.sprememba.rezultata", height = 600, width=570))),
-#     
-#     tabPanel("Zemljevid",
-#              sidebarPanel(
-#                uiOutput("spol")),
-#              
-#              sidebarPanel(  
-#                uiOutput("leto")),
-#              
-#              sidebarPanel(
-#                uiOutput("disciplina")),
-#              leafletOutput("mymap", height = 500))
-#     
-#     
-#     
-#   )
-#   
-# ))
 library(shiny)
 
 shinyUI(fluidPage(
@@ -36,18 +5,22 @@ shinyUI(fluidPage(
   titlePanel("Rezultati svetovnih prvenstev"),
   
   tabsetPanel(
-    tabPanel("Graf",
+    tabPanel("Graf spremembe rezultatov glede na leto 2005",
              sidebarPanel(
                
-               uiOutput("spol.graf"),
-               checkboxGroupInput(inputId="discipline", label = "Izberi disciplino:",
-                                  choiceNames=c(unique(rezultati$disciplina)),
-                                  choiceValues =c(unique(rezultati$disciplina)))
-             ),
+               checkboxGroupInput(inputId="discipline_tehnicne", label = "Izberi tehnicno disciplino:",
+                                  choiceNames=c(unique(rezultati.tehnicne$disciplina)),
+                                  choiceValues =c(unique(rezultati.tehnicne$disciplina))),
+               
+               checkboxGroupInput(inputId="discipline_tekaske", label = "Izberi tekasko disciplino:",
+                                  choiceNames=c(unique(rezultati.tekaske$disciplina)),
+                                  choiceValues =c(unique(rezultati.tekaske$disciplina)), selected  ="100 m")),
+             
+             sidebarPanel(uiOutput("spol.graf")),
              
              mainPanel(plotOutput("graf.sprememba.rezultata", height = 500, width=500))),
     
-    tabPanel("Zemljevid",
+    tabPanel("Iz kje prihajajo filanisti šprinterskih disciplin ",
              sidebarPanel(
                uiOutput("spol")),
              
