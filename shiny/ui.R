fe3c0d9d97680da2f6e808febac626fce637559f
larajagodnik <lara.jagodnik@gmail.com>
2019-03-19 21:23:16 +0100

    poskus drugačnega zbiranja disciplin

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,3 +1,34 @@
+# library(shiny)
+# 
+# shinyUI(fluidPage(
+#   
+#   titlePanel("Rezultati svetovnih prvenstev"),
+#   
+#   tabsetPanel(
+#     tabPanel("Graf",
+#              sidebarPanel(
+#                
+#                uiOutput("spol.graf"),
+#                uiOutput("tip"),
+#                uiOutput("disciplina.graf")),
+#              mainPanel(plotOutput("graf.sprememba.rezultata", height = 600, width=570))),
+#     
+#     tabPanel("Zemljevid",
+#              sidebarPanel(
+#                uiOutput("spol")),
+#              
+#              sidebarPanel(  
+#                uiOutput("leto")),
+#              
+#              sidebarPanel(
+#                uiOutput("disciplina")),
+#              leafletOutput("mymap", height = 500))
+#     
+#     
+#     
+#   )
+#   
+# ))
 library(shiny)
 
 shinyUI(fluidPage(
@@ -8,10 +39,13 @@ shinyUI(fluidPage(
     tabPanel("Graf",
              sidebarPanel(
                
-               uiOutput("spol.graf"),
-               uiOutput("tip"),
-               uiOutput("disciplina.graf")),
-             mainPanel(plotOutput("graf.sprememba.rezultata", height = 600, width=570))),
+               checkboxGroupInput(inputId="discipline", label = "Izberi disciplino:",
+                                  choiceNames=c(unique(rezultati$disciplina)),
+                                  choiceValues =c(unique(rezultati$disciplina)))
+             ),
+             
+             uiOutput("spol.graf"),
+             mainPanel(plotOutput("graf.sprememba.rezultata", height = 500, width=500))),
     
     tabPanel("Zemljevid",
              sidebarPanel(
